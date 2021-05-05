99eb7a712a30cb577dad0addeb7b580d25c6fbfd
nilgf23 <82875774+nilgf23@users.noreply.github.com>
2021-05-05 09:42:16 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,20 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    led.plot(x, y)
+    x += 1
+    if (x == 5) {
+        x = -1
+        y += 1
+    }
 })
+input.onButtonPressed(Button.B, function () {
+    led.unplot(x, y)
+    x += -1
+    if (x == -1) {
+        x = 5
+        y += -1
+    }
+})
+let y = 0
+let x = 0
+x = -1
+y = 0
